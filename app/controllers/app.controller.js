3286c3f3dac42f428b52610238428b2a7bcbb4ed
Osman Pérez <operez@edutecno.com>
2023-07-28 21:40:13 -0400

    Consultas de Cursos y Participantes

--- app/controllers/app.controller.js
+++ app/controllers/app.controller.js
@@ -23,3 +23,51 @@ exports.crearCurso = async (curso) => {
         return null
     }
 }
+
+exports.agregarParticipante = async (idCurso, idParticipante) => {
+    const curso = await Curso.findByPk(idCurso)
+    if(!curso) { // Validación para verificar existencia del curso
+        console.log("Curso no encontrado "+idCurso);
+        return null
+    }
+    const participante = await Participante.findByPk(idParticipante)
+    if(!participante) { //Validación para verificar la existencia del participante
+        console.log("Participante no encontrado "+idParticipante);
+        return null
+    }
+    await curso.addParticipante(participante)
+    console.log(`Participante ${idParticipante} agregado al curso ${idCurso}`);
+    return curso;
+}
+
+exports.consultarCurso = async (idCurso) => {
+    const curso = await Curso.findByPk(idCurso, {
+        include: [
+            {
+                model: Participante,
+                as: "participantes",
+                attributes: [ "id","rut","nombre"],
+                through: {
+                    attributes: ["curso_id","participante_id"]
+                }
+            }
+        ]
+    })
+    return curso
+}
+
+exports.consultarParticipante = async (idParticipante) => {
+    const participante = await Participante.findByPk(idParticipante, {
+        include: [
+            {
+                model: Curso,
+                as: "cursos",
+                attributes: [ "id","nombre"],
+                through: {
+                    attributes: ["curso_id","participante_id"]
+                }
+            }
+        ]
+    })
+    return participante
+}
--- server.js
+++ server.js
@@ -10,9 +10,24 @@ const main = async() => {
     // const participante4 = await controller.crearParticipante({ rut: "44.444.444-4", nombre: "Pablo"})
 
     // Registro de Cursos
-    const curso1 = await controller.crearCurso({ nombre: "Javascript"})
-    const curso2 = await controller.crearCurso({ nombre: "Python"})
-    const curso3 = await controller.crearCurso({ nombre: "Java"})
+    // const curso1 = await controller.crearCurso({ nombre: "Javascript"})
+    // const curso2 = await controller.crearCurso({ nombre: "Python"})
+    // const curso3 = await controller.crearCurso({ nombre: "Java"})
+
+    // Asociar Participantes a cursos
+    await controller.agregarParticipante(1,1)
+    await controller.agregarParticipante(1,4)
+    await controller.agregarParticipante(2,3)
+    await controller.agregarParticipante(3,1)
+    await controller.agregarParticipante(3,2)
+    await controller.agregarParticipante(3,3)
+
+
+    const infoCurso = await controller.consultarCurso(1)
+    console.log("InfoCurso", JSON.stringify(infoCurso, null,4));
+
+    const infoParticipante = await controller.consultarParticipante(4)
+    console.log("InfoParticipante", JSON.stringify(infoParticipante, null, 4));
     
     console.log("Ejecución exitosa");
 }
